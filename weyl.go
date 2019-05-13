9c477bc4bac183b8134c71d92296ad9397fedbc9
tidwall <joshbaker77@gmail.com>
2019-05-13 13:04:58 -0700

    Export Generator

--- weyl.go
+++ weyl.go
@@ -5,38 +5,38 @@ import (
 	"sync"
 )
 
-type weyl struct {
+type Generator struct {
 	x, w, s uint64
 }
 
-func (w *weyl) Uint64() uint64 {
+func (w *Generator) Uint64() uint64 {
 	w.w += w.s
 	w.x = ((w.x*w.x + w.w) >> 32) | ((w.x*w.x + w.w) << 32)
 	return w.x
 }
 
-func (w *weyl) Int63() int64 {
+func (w *Generator) Int63() int64 {
 	return int64(w.Uint64() << 1 >> 1)
 }
 
-func (w *weyl) Seed(seed int64) {
-	*w = weyl{s: (uint64(seed) << 1) + 0xb5ad4eceda1ce2a9}
+func (w *Generator) Seed(seed int64) {
+	*w = Generator{s: (uint64(seed) << 1) + 0xb5ad4eceda1ce2a9}
 }
 
 func NewSource(seed int64) rand.Source {
-	var w weyl
+	var w Generator
 	w.Seed(seed)
 	return &w
 }
 
 func NewSource64(seed int64) rand.Source64 {
-	var w weyl
+	var w Generator
 	w.Seed(seed)
 	return &w
 }
 
 var mu sync.Mutex
-var w weyl
+var w Generator
 
 func Int63() int64 {
 	mu.Lock()
